c696e2994cf92deaaddd13424e6439efa8553653
sangeethaRamkumar <sangiammu1020@gmail.com>
2023-09-27 14:52:18 +0530

    useContext in functional component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import './App.css';
-import FunctionalContextComponent from './FunctionalContextComponent';
+import "./App.css";
+import React, { useState } from "react";
+import FunctionalContextComponent from "./FunctionalContextComponent";
 
+export const ThemeContext = React.createContext();
 
 function App() {
+ const [darkTheme, setDarkTheme]=useState(true)
+
+  const toggleTheme = () => {
+    setDarkTheme(prevDarkTheme=> !prevDarkTheme)
+    console.log(darkTheme);
+  };
+
   return (
-    <div className="App">
-
-    <FunctionalContextComponent />
-
-    </div>
+    <>
+      <ThemeContext.Provider value={darkTheme}>
+        <button onClick={toggleTheme}> Toggle Theme</button>
+        <FunctionalContextComponent />
+      </ThemeContext.Provider>
+    </>
   );
 }
 
--- src/FunctionalContextComponent.js
+++ src/FunctionalContextComponent.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {ThemeContext} from './App'
 
 export default function FunctionalContextComponent() {
+
+const darkTheme=useContext(ThemeContext)
+
+const darkStyle={
+    padding:'2rem',
+    margin:'2rem',
+    backgroundColor:darkTheme? '#333' : '#ccc',
+    color:darkTheme? '#ccc' : '#333'
+    
+}
+
   return (
-    <div>
+    <div style={darkStyle}>
+       <h1>
         FunctionalContextComponent
+        </h1> 
     </div>
   )
 }
