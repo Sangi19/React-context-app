7bb7b8d4e1399d00bae14aa996e08ff44ca247cc
sangeethaRamkumar <sangiammu1020@gmail.com>
2023-09-27 14:51:05 +0530

    functional component create

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import FunctionalContextComponent from './FunctionalContextComponent';
+
+
+function App() {
+  return (
+    <div className="App">
+
+    <FunctionalContextComponent />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FunctionalContextComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {ThemeContext} from './App'
+
+export default function FunctionalContextComponent() {
+  return (
+    <div>
+        FunctionalContextComponent
+    </div>
+  )
+}
